9fc2680cc26cfe79e5e8c64d69aa3543700709db
Argentcode <CraigOrman0@gmail.com>
2026-07-25 13:29:09 -0500

    basic useability

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 def main():
     # Your core program logic goes here
-    print("Hello from the main function!")
+    print("Welcome to Galactic Expansions")
+    print("Name your empire: ")
+    name = input("Name your empire: ")
+    newPlayer = Player(name)
+    while True:
+       status(newPlayer)
+       choice = input("What would you like to do? m to upgrade metal mine, c to upgrade crystal mine, x to exit ")
+       if choice == "x":
+          print("Thanks for playing!")
+          exit()
+       elif choice == "m":
+          print("Metal Mine upgraded!")
+       elif choice == "c":
+          print("Crystal Mine upgraded!")
+       elif choice == "m":
+          print("I didn't quite get that. Try again,")
+
+
+def status(Player):
+   print("Metal:", Player.metal, " Crystal:", Player.crystal, " Energy:", Player.energy)
+   print("Metal Mine:", Player.metalmine.level, "Crystal Mine:", Player.crystalmine.level)
+
 
 class Mine:
   def __init__(self, rate, level, name):
     self.level = level
     self.name = name
     self.rate = rate * self.level
+    self.energyConsumption = 5 * self.level
     self.upgradeMetalCost = 100 * self.level
     self.updateCrystalCost = 75 * self.level
     self.upgradeTime = 10 * self.level
 
 
 class MetalMine:
-   def __init__(self, rate, level, name):
-      super().__init__(self, 50, level, "Metal")
+   def __init__(self, level=1):
+      Mine.__init__(self, 50, level, "Metal")
 
 
-class MetalMine:
-   def __init__(self, rate, level, name):
-      super().__init__(self, 35, level, "Crystal")
+class CrystalMine:
+   def __init__(self, level=1):
+      Mine.__init__(self, 35, level, "Crystal")
 
 
-class ship:
+class Ship:
   def __init__(self, name, hitpoints, armor, damage, type):
    self.name = name
    self.hitpoints = hitpoints
@@ -30,6 +52,15 @@ class ship:
    self.damage = damage
    self.type = type
 
+
+class Player:
+   def __init__(self, name):
+      self.name = name
+      self.metalmine = MetalMine(1)
+      self.crystalmine = CrystalMine(1)
+      self.metal = 1000
+      self.crystal = 1000
+      self.energy = 50
 
 
 if __name__ == "__main__":
